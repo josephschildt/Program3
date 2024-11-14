4887b3670b2980775fc0542f04f76066504d1e43
Daulton Widlacki <soldbuydaulton@gmail.com>
2024-11-14 13:52:45 -0600

    cleaned up pieces

--- bishop.py
+++ bishop.py
@@ -1,117 +1,99 @@
 from chess_piece import ChessPiece
 from board import Board
-from chess_utils import BoardInfo
-from chess_utils import PieceInfo
+from chess_utils import BoardInfo, PieceInfo
+
 
 class Bishop(ChessPiece):
-  
-  def __init__(self, row_num, col_num, color, label):
-      ChessPiece.__init__(self, row_num, col_num, BoardInfo.BLACK, PieceInfo.BLACK)
-      self._color = color
-      self._label = label
+    def __init__(self, row_num, col_num, color, label):
+        ChessPiece.__init__(self, row_num, col_num,
+                            BoardInfo.BLACK, PieceInfo.BLACK)
+        self._color = color
+        self._label = label
 
-  def is_legal_move(self, dest_row, dest_col, board):
-      row_diff = abs(self._row - dest_row)
-      col_diff = abs(self._col - dest_col)
-      square_type = board.get_square_info(dest_row, dest_col)
+    def is_legal_move(self, dest_row, dest_col, board):
+        """Check if the bishop's move to the destination square is legal."""
+        if not self._is_valid_starting_position(dest_row, dest_col):
+            return False
 
-    #   if dest_row == self._row and dest_col == self._col:
-    #         return True
+        if not self._is_valid_diagonal_move(dest_row, dest_col):
+            return False
 
-    #     # Ensure move is diagonal
-    #   if abs(self._row - dest_row) != abs(self._col - dest_col):
-    #         return False
+        if not self._is_valid_destination(dest_row, dest_col, board):
+            return False
 
+        return self._is_path_clear(dest_row, dest_col, board)
 
-    #   # make sure the move is diagonal
-    #   is_legal = (row_diff == col_diff) and square_type != self._color and square_type != BoardInfo.OFF_THE_BOARD
-    #   return is_legal
-      if dest_row == self._row and dest_col == self._col:
-        return False
+    def _is_valid_starting_position(self, dest_row, dest_col):
+        """Check if the move isn't to the same square."""
+        return not (dest_row == self._row and dest_col == self._col)
 
-    # Check if the move is diagonal (row difference == column difference)
-      if row_diff != col_diff:
-        return False
+    def _is_valid_diagonal_move(self, dest_row, dest_col):
+        """Check if the move is diagonal."""
+        row_diff = abs(self._row - dest_row)
+        col_diff = abs(self._col - dest_col)
+        return row_diff == col_diff
 
-    # Check if the destination is off the board or occupied by a piece of the same color
-      if square_type == self._color or square_type == BoardInfo.OFF_THE_BOARD:
-        return False
+    def _is_valid_destination(self, dest_row, dest_col, board):
+        """Check if the destination square is valid."""
+        square_type = board.get_square_info(dest_row, dest_col)
+        return (square_type != self._color and
+                square_type != BoardInfo.OFF_THE_BOARD)
 
-    # Check if there are any pieces blocking the path for diagonal movement
-      row_step = 1 if dest_row > self._row else -1
-      col_step = 1 if dest_col > self._col else -1
+    def _is_path_clear(self, dest_row, dest_col, board):
+        """Check if the diagonal path to destination is clear of pieces."""
+        row_step = 1 if dest_row > self._row else -1
+        col_step = 1 if dest_col > self._col else -1
 
-    # Traverse along the diagonal and check for any pieces in the way
-      curr_row, curr_col = self._row + row_step, self._col + col_step
-      while (curr_row != dest_row) and (curr_col != dest_col):
-        if board.get_square_info(curr_row, curr_col) != BoardInfo.EMPTY:
-            return False  # Path is blocked by another piece
-        curr_row += row_step
-        curr_col += col_step
+        curr_row = self._row + row_step
+        curr_col = self._col + col_step
 
-      return True  # The move is legal
-  
+        while (curr_row != dest_row) and (curr_col != dest_col):
+            if board.get_square_info(curr_row, curr_col) != BoardInfo.EMPTY:
+                return False
+            curr_row += row_step
+            curr_col += col_step
 
-# def is_legal_move(self, dest_row, dest_col, board):
-#         """Check if the bishop move to dest_row, dest_col is legal, including staying in place."""
-#         # Allow bishop to stay in place
-#         if dest_row == self._row and dest_col == self._col:
-#             return True
+        return True
 
-#         # Ensure move is diagonal
-#         if abs(self._row - dest_row) != abs(self._col - dest_col):
-#             return False
-
-#         # Check for obstacles in the path
-#         row_step = 1 if dest_row > self._row else -1
-#         col_step = 1 if dest_col > self._col else -1
-
-#         current_row, current_col = self._row + row_step, self._col + col_step
-#         while current_row != dest_row or current_col != dest_col:
-#             if board._board_info[current_row][current_col] is not None:
-#                 return False
-#             current_row += row_step
-#             current_col += col_step
-
-#         # Check if destination is empty or has an opponent piece
-#         destination_piece = board._board_info[dest_row][dest_col]
-#         return destination_piece is None or destination_piece.get_color() != self.get_color()
-
-
-  def generate_legal_moves(self, board_data, board):
+    def generate_legal_moves(self, board_data, board):
+        """Generate all legal moves for the bishop."""
         char_label = self._label.value
         board_data[self._row][self._col] = char_label
 
-     # Diagonal directions
         directions = [
-        (-1, -1),  # top-left
-        (-1, 1),   # top-right
-        (1, -1),   # bottom-left
-        (1, 1)     # bottom-right
+            (-1, -1),  # top-left
+            (-1, 1),   # top-right
+            (1, -1),   # bottom-left
+            (1, 1)     # bottom-right
         ]
 
-        for row_direction, col_direction in directions:
-            new_row = self._row
-            new_col = self._col
-
-        # Keep going in this direction until blocked
-            while True:
-                new_row += row_direction
-                new_col += col_direction
-
-                if new_row < 0 or new_row >= 8 or new_col < 0 or new_col >= 8:
-                    break  # Stop if it's off the board
-            
-                square_type = board.get_square_info(new_row, new_col)
-
-                if square_type != BoardInfo.EMPTY:
-                    if square_type != self._color:  # If it's an enemy piece, it's a legal move
-                        board_data[new_row][new_col] = char_label  # Mark the legal move
-                    break  # Stop if we hit any piece (friend or enemy)
-            
-
-                board_data[new_row][new_col] = char_label  # Mark the empty square as a legal move
+        for direction in directions:
+            self._mark_moves_in_direction(board_data, board, direction)
 
         return board_data
-      
-    
+
+    def _mark_moves_in_direction(self, board_data, board, direction):
+        """Mark all legal moves in a given direction."""
+        row_direction, col_direction = direction
+        new_row = self._row
+        new_col = self._col
+
+        while True:
+            new_row += row_direction
+            new_col += col_direction
+
+            if not self._is_within_board(new_row, new_col):
+                break
+
+            square_type = board.get_square_info(new_row, new_col)
+
+            if square_type != BoardInfo.EMPTY:
+                if square_type != self._color:
+                    board_data[new_row][new_col] = self._label.value
+                break
+
+            board_data[new_row][new_col] = self._label.value
+
+    def _is_within_board(self, row, col):
+        """Check if the given position is within the board boundaries."""
+        return 0 <= row < 8 and 0 <= col < 8
--- knight.py
+++ knight.py
@@ -1,4 +1,3 @@
-
 from chess_piece import ChessPiece
 from board import Board
 from chess_utils import BoardInfo
@@ -6,46 +5,57 @@ from chess_utils import PieceInfo
 
 
 class Knight(ChessPiece):
+    def __init__(self, row_num, col_num, color, label):
+        super().__init__(row_num, col_num, color, label)
+        self._color = color
+        self._label = label
 
-  def __init__(self, row_num, col_num, color, label):
-    # ChessPiece.__init__(self, row_num, col_num, BoardInfo.BLACK, PieceInfo.BLACK)
-    ChessPiece.__init__(self, row_num, col_num, color, label)
-    self._color = color
-    self._label = label
-    
+    def _get_knight_moves(self):
+        """Returns all possible knight move patterns."""
+        return [
+            (-2, -1), (-2, 1),  # Move two rows up, one column left/right
+            (-1, -2), (-1, 2),  # Move one row up, two columns left/right
+            (1, -2), (1, 2),    # Move one row down, two columns left/right
+            (2, -1), (2, 1)     # Move two rows down, one column left/right
+        ]
 
+    def _is_valid_knight_pattern(self, row_diff, col_diff):
+        """Checks if the move follows valid knight movement pattern."""
+        return (row_diff == 2 and col_diff == 1) or (row_diff == 1 and col_diff == 2)
 
-  def is_legal_move(self, dest_row, dest_col, board):
-    row_diff = abs(self._row - dest_row)
-    col_diff = abs(self._col - dest_col)
-    square_type = board.get_square_info(dest_row, dest_col)
-    
-    is_legal = ((row_diff == 2 and col_diff == 1) or (row_diff == 1 and col_diff == 2)) and square_type != self._color and square_type != BoardInfo.OFF_THE_BOARD
+    def _is_valid_destination(self, dest_row, dest_col, board):
+        """Checks if the destination square is valid (not off board or same color)."""
+        square_type = board.get_square_info(dest_row, dest_col)
+        return square_type != self._color and square_type != BoardInfo.OFF_THE_BOARD
 
-    return is_legal
+    def _is_within_board(self, row, col):
+        """Checks if the given position is within the board boundaries."""
+        return 0 <= row < 8 and 0 <= col < 8
 
- 
-  def generate_legal_moves(self, board_data, board):
-    char_label = self._label.value
-    board_data[self._row][self._col] = char_label
+    def is_legal_move(self, dest_row, dest_col, board):
+        """Determines if a move to the destination square is legal."""
+        row_diff = abs(self._row - dest_row)
+        col_diff = abs(self._col - dest_col)
 
-    directions = [
-      (-2, -1), (-2, 1),  # Move two rows up, one column left/right
-      (-1, -2), (-1, 2),  # Move one row up, two columns left/right
-      (1, -2), (1, 2),    # Move one row down, two columns left/right
-      (2, -1), (2, 1)     # Move two rows down, one column left/right
-      ]
+        return (self._is_valid_knight_pattern(row_diff, col_diff) and
+                self._is_valid_destination(dest_row, dest_col, board))
 
-    for row_direction, col_direction in directions:
-            new_row = self._row + row_direction
-            new_col = self._col + col_direction
+    def _calculate_new_position(self, direction):
+        """Calculates new position based on movement direction."""
+        row_direction, col_direction = direction
+        return (self._row + row_direction, self._col + col_direction)
 
-            # Check if the move is on the board
-            if 0 <= new_row < 8 and 0 <= new_col < 8:
-                square_type = board.get_square_info(new_row, new_col)
+    def generate_legal_moves(self, board_data, board):
+        """Generates all legal moves for the knight on the current board."""
+        # Mark current position
+        board_data[self._row][self._col] = self._label.value
 
-                # Add as a legal move if it's an empty square or occupied by an enemy
-                if square_type != self._color and square_type != BoardInfo.OFF_THE_BOARD:
-                    board_data[new_row][new_col] = char_label
+        # Check all possible knight moves
+        for direction in self._get_knight_moves():
+            new_row, new_col = self._calculate_new_position(direction)
 
-    return board_data
+            if (self._is_within_board(new_row, new_col) and
+                    self._is_valid_destination(new_row, new_col, board)):
+                board_data[new_row][new_col] = self._label.value
+
+        return board_data
