94ee91a19236542f61c6f30903e2093790409881
jdschil <jdschil@aspen.ad.ilstu.edu>
2024-11-14 13:06:43 -0600

    rough draft

--- bishop.py
+++ bishop.py
@@ -15,17 +15,43 @@ class Bishop(ChessPiece):
       col_diff = abs(self._col - dest_col)
       square_type = board.get_square_info(dest_row, dest_col)
 
+    #   if dest_row == self._row and dest_col == self._col:
+    #         return True
+
+    #     # Ensure move is diagonal
+    #   if abs(self._row - dest_row) != abs(self._col - dest_col):
+    #         return False
+
+
+    #   # make sure the move is diagonal
+    #   is_legal = (row_diff == col_diff) and square_type != self._color and square_type != BoardInfo.OFF_THE_BOARD
+    #   return is_legal
       if dest_row == self._row and dest_col == self._col:
-            return True
+        return False
 
-        # Ensure move is diagonal
-      if abs(self._row - dest_row) != abs(self._col - dest_col):
-            return False
+    # Check if the move is diagonal (row difference == column difference)
+      if row_diff != col_diff:
+        return False
 
+    # Check if the destination is off the board or occupied by a piece of the same color
+      if square_type == self._color or square_type == BoardInfo.OFF_THE_BOARD:
+        return False
 
-      # make sure the move is diagonal
-      is_legal = (row_diff == col_diff) and square_type != self._color and square_type != BoardInfo.OFF_THE_BOARD
-      return is_legal
+    # Check if there are any pieces blocking the path for diagonal movement
+      row_step = 1 if dest_row > self._row else -1
+      col_step = 1 if dest_col > self._col else -1
+
+    # Traverse along the diagonal and check for any pieces in the way
+      curr_row, curr_col = self._row + row_step, self._col + col_step
+      while (curr_row != dest_row) and (curr_col != dest_col):
+        if board.get_square_info(curr_row, curr_col) != BoardInfo.EMPTY:
+            return False  # Path is blocked by another piece
+        curr_row += row_step
+        curr_col += col_step
+
+      return True  # The move is legal
+  
+
 # def is_legal_move(self, dest_row, dest_col, board):
 #         """Check if the bishop move to dest_row, dest_col is legal, including staying in place."""
 #         # Allow bishop to stay in place
