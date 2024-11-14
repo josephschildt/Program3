39a72f04f4f2760ab9eb9fc58bd73181bcd23f04
Daulton Widlacki <soldbuydaulton@gmail.com>
2024-11-14 13:39:09 -0600

    rook stuff

--- rook.py
+++ rook.py
@@ -3,93 +3,97 @@ from board import Board
 from chess_utils import BoardInfo
 from chess_utils import PieceInfo
 
+
 class Rook(ChessPiece):
+    def __init__(self, row_num, col_num, color, label):
+        ChessPiece.__init__(self, row_num, col_num,
+                            BoardInfo.BLACK, PieceInfo.BLACK)
+        self._color = color
+        self._label = label
 
-  def __init__(self, row_num, col_num, color, label):
-    ChessPiece.__init__(self, row_num, col_num, BoardInfo.BLACK, PieceInfo.BLACK)
-    self._color = color
-    self._label = label
+    def _is_same_position(self, dest_row, dest_col):
+        """Check if the destination is the same as current position."""
+        return dest_row == self._row and dest_col == self._col
 
+    def _is_straight_line_move(self, row_diff, col_diff):
+        """Check if the move is horizontal or vertical."""
+        return (row_diff > 0 and col_diff == 0) or (row_diff == 0 and col_diff > 0)
 
-  def is_legal_move(self, dest_row, dest_col, board):
-    
-    # if dest_row == self._row and dest_col == self._col:
-    #         return True
-    
-    # row_diff = abs(self._row - dest_row)
-    # col_diff = abs(self._col - dest_col)
-    # #check the diffs
-    
-    # # need to make a file called my_pieces.py and put all of the stuff in there 
-    # # need to make it a check so the rook can choose not to move aswell, needed for all most likely
-    # square_type = board.get_square_info(dest_row, dest_col)
-    
-    # is_legal = ((row_diff > 0 and col_diff == 0) or (row_diff == 0 and col_diff > 0)) and square_type != self._color and square_type != BoardInfo.OFF_THE_BOARD
+    def _get_direction(self, dest_pos, current_pos):
+        """Calculate the direction of movement (+1, 0, or -1)."""
+        if dest_pos == current_pos:
+            return 0
+        return 1 if dest_pos > current_pos else -1
 
-    # return is_legal
-      if dest_row == self._row and dest_col == self._col:
-            return True  # The rook can stay in place.
+    def _is_valid_destination(self, square_type):
+        """Check if the destination square is valid to move to."""
+        return square_type != self._color and square_type != BoardInfo.OFF_THE_BOARD
 
-      row_diff = abs(self._row - dest_row)
-      col_diff = abs(self._col - dest_col)
+    def _is_path_clear(self, dest_row, dest_col, board):
+        """Check if there are any pieces blocking the path to destination."""
+        row_direction = self._get_direction(dest_row, self._row)
+        col_direction = self._get_direction(dest_col, self._col)
 
-      # Check if the move is horizontal or vertical
-      if not ((row_diff > 0 and col_diff == 0) or (row_diff == 0 and col_diff > 0)):
-            return False  # Rook can only move horizontally or vertically.
+        current_row = self._row + row_direction
+        current_col = self._col + col_direction
 
-      # Check for obstacles in the path
-      row_direction = 0 if self._row == dest_row else (1 if dest_row > self._row else -1)
-      col_direction = 0 if self._col == dest_col else (1 if dest_col > self._col else -1)
-
-      current_row = self._row + row_direction
-      current_col = self._col + col_direction
-      while current_row != dest_row or current_col != dest_col:
-            square_type = board.get_square_info(current_row, current_col)
-            if square_type != BoardInfo.EMPTY:
-                return False  # Path is blocked by a piece
+        while current_row != dest_row or current_col != dest_col:
+            if board.get_square_info(current_row, current_col) != BoardInfo.EMPTY:
+                return False
             current_row += row_direction
             current_col += col_direction
+        return True
 
-        # Destination square must be empty or occupied by an enemy piece
-      square_type = board.get_square_info(dest_row, dest_col)
+    def is_legal_move(self, dest_row, dest_col, board):
+        # Check if staying in place
+        if self._is_same_position(dest_row, dest_col):
+            return True
 
-      return square_type != self._color and square_type != BoardInfo.OFF_THE_BOARD
-  
+        row_diff = abs(self._row - dest_row)
+        col_diff = abs(self._col - dest_col)
 
-  def generate_legal_moves(self, board_data, board):
-    char_label = self._label.value
-    board_data[self._row][self._col] = char_label
+        # Check if move is horizontal or vertical
+        if not self._is_straight_line_move(row_diff, col_diff):
+            return False
 
-    # up down left right
-    directions = [
-      (-1, 0),
-      (1, 0),
-      (0, -1),
-      (0, 1)
-    ]
-    
-    for row_direction, col_direction in directions:
+        # Check for obstacles and valid destination
+        return (self._is_path_clear(dest_row, dest_col, board) and
+                self._is_valid_destination(board.get_square_info(dest_row, dest_col)))
+
+    def _get_moves_in_direction(self, row_direction, col_direction, board_data, board):
+        """Generate legal moves in a specific direction."""
         new_row = self._row
         new_col = self._col
 
-        # Keep going in this direction until blocked
         while True:
             new_row += row_direction
             new_col += col_direction
-            
-            # Check if we are off the board
-            if new_row < 0 or new_row >= 8 or new_col < 0 or new_col >= 8:
-                break  # Stop if it's off the board
-            
+
+            # Check board boundaries
+            if not (0 <= new_row < 8 and 0 <= new_col < 8):
+                break
+
             square_type = board.get_square_info(new_row, new_col)
 
-            # If we hit any piece (friend or enemy)
             if square_type != BoardInfo.EMPTY:
-                if square_type != self._color:  # If it's an enemy piece, it's a legal move
-                    board_data[new_row][new_col] = char_label  # Mark the legal move
-                break  # Stop if we hit any piece (friend or enemy)
-            else:
-                board_data[new_row][new_col] = char_label  # Mark the empty square as a legal move
+                if square_type != self._color:
+                    board_data[new_row][new_col] = self._label.value
+                break
 
-    return board_data
+            board_data[new_row][new_col] = self._label.value
 
+        return board_data
+
+    def generate_legal_moves(self, board_data, board):
+        # Mark current position
+        board_data[self._row][self._col] = self._label.value
+
+        # Define directions: up, down, left, right
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+        # Generate moves for each direction
+        for row_direction, col_direction in directions:
+            board_data = self._get_moves_in_direction(row_direction, col_direction,
+                                                      board_data, board)
+
+        return board_data
