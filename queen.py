f75bf0ca37d2643e628780034f28584b087237ff
josephschildt <schildtjoey@gmail.com>
2024-11-14 13:34:18 -0600

    working queen

--- queen.py
+++ queen.py
@@ -12,13 +12,37 @@ class Queen(ChessPiece):
 
 
   def is_legal_move(self, dest_row, dest_col, board):
-      row_diff = abs(self._row - dest_row)
-      col_diff = abs(self._col - dest_col) # maybe take out the abs since they can move backwards as well
-      square_type = board.get_square_info(dest_row, dest_col)
+    #   row_diff = abs(self._row - dest_row)
+    #   col_diff = abs(self._col - dest_col) # maybe take out the abs since they can move backwards as well
+    #   square_type = board.get_square_info(dest_row, dest_col)
 
-      # moves can essentially be anywhere, straight, diag, can go back or forwards
-      is_legal = (row_diff == col_diff) or ((row_diff > 0 and col_diff == 0) or (row_diff == 0 and col_diff > 0)) and square_type != self._color and square_type != BoardInfo.OFF_THE_BOARD
-      return is_legal
+    #   # moves can essentially be anywhere, straight, diag, can go back or forwards
+    #   is_legal = (row_diff == col_diff) or ((row_diff > 0 and col_diff == 0) or (row_diff == 0 and col_diff > 0)) and square_type != self._color and square_type != BoardInfo.OFF_THE_BOARD
+    #   return is_legal
+    if dest_row == self._row and dest_col == self._col:
+            return True
+
+        # Ensure move is either straight or diagonal
+    if self._row != dest_row and self._col != dest_col and abs(self._row - dest_row) != abs(self._col - dest_col):
+            return False
+
+        # Determine movement direction
+    row_step = 0 if self._row == dest_row else (
+            1 if dest_row > self._row else -1)
+    col_step = 0 if self._col == dest_col else (
+            1 if dest_col > self._col else -1)
+
+        # Check for obstacles along the path
+    current_row, current_col = self._row + row_step, self._col + col_step
+    while current_row != dest_row or current_col != dest_col:
+            if board._board_info[current_row][current_col] is not None:
+                return False
+            current_row += row_step
+            current_col += col_step
+
+        # Check if destination is empty or has an opponent piece
+    destination_piece = board._board_info[dest_row][dest_col]
+    return destination_piece is None or destination_piece.get_color() != self.get_color()
   
 
   def generate_legal_moves(self, board_data, board):
